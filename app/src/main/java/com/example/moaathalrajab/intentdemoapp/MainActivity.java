3e24fef0883060812ecd40127b7c174623b49385
Moaath Alrajab <moaath@alrajab.net>
2018-12-17 02:14:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/moaathalrajab/intentdemoapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.moaathalrajab.intentdemoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startExplicitIntent(View view) {
+
+    }
+
+    public void startImplicitIntent(View view) {
+
+    }
+
+}
